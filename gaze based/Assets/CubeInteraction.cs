fcf03bbde2b8ecc916ae79b60863bed7b098422e
Jagaddeesh Krishna S Nunna <66049652+JagadeeshKrishnaSNunna@users.noreply.github.com>
2021-03-06 11:54:40 +0530

    gaze

---
+++ gaze based/Assets/CubeInteraction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class CubeInteraction : MonoBehaviour
+{
+    private float timer;
+    const float totalTime=2f;
+    bool isGazed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGazed){
+            timer +=Time.deltaTime;
+            if(timer>totalTime){
+                timer=0;
+                PointerDown();
+                GetComponent<Collider>().enabled=false;
+                GetComponent<AudioSource>().Play();
+
+            }
+        }
+        
+    }
+    public void PointerEnter(){
+        isGazed=true;
+        Debug.Log("Enter");
+    }
+    public void PointerExit(){
+        Debug.Log("exit");
+        isGazed=false;
+    }
+    public void PointerDown(){
+        Debug.Log("Done");
+    }
+}
